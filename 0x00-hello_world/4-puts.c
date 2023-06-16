4a68f3bb3038aaf5c35b284d13f680f3a0d8e0f8
keigi14 <kbndbrn5@gmail.com>
2023-06-16 06:35:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Return: 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
