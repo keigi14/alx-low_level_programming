68e28758aca83ea3e499c9afc0a32693d3d8ebe5
keigi14 <kbndbrn5@gmail.com>
2023-11-23 11:44:56 -0800

    this is every task's solution on bit-manipulation project

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of given bit at 0
+ * @n: ptr to the number we gonna change
+ * @index: index of bit to clear
+ * Return: 1 on success, -1 on failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the nubr of bits to be changed
+ * to get from one nubr to another
+ * @n: first nubr
+ * @m: second nubr
+ * Return: num of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
