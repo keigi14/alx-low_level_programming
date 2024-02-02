fd331765681238d767c0d21540a0268745986aac
keigi14 <kbndbrn5@gmail.com>
2024-02-02 12:39:26 -0800

    its the last push

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,12 +1,5 @@
 #include "hash_tables.h"
 
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
-
 /**
  * shash_table_create - Creates a sorted hash table.
  * @size: de size of new sorted hash table.
@@ -17,7 +10,7 @@ void shash_table_delete(shash_table_t *ht);
 shash_table_t *shash_table_create(unsigned long int size)
 {
 	shash_table_t *ht;
-	unsigned long int i;
+	unsigned long int in;
 
 	ht = malloc(sizeof(shash_table_t));
 	if (ht == NULL)
@@ -27,8 +20,8 @@ shash_table_t *shash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(shash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	for (in = 0; in < size; in++)
+		ht->array[in] = NULL;
 	ht->shead = NULL;
 	ht->stail = NULL;
 
